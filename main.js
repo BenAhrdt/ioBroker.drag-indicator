0ebc44277dc73820e12a98ea25230d26322dfdb5
BenAhrdt <bsahrdt@gmail.com>
2022-04-08 00:38:58 +0200

    change format of timestamp

--- main.js
+++ main.js
@@ -334,8 +334,8 @@ class DragIndicator extends utils.Adapter {
 		const day = cur.getDate();
 		const hour = cur.getHours();
 		const minute = cur.getMinutes();
-		const seconds = cur.getSeconds();
-		return `${hour}:${minute}:${seconds} ${day}.${month}.${year}`;
+		const second = cur.getSeconds();
+		return `${year}.${month}.${day} - ${hour}.${minute}.${second}`;
 	}
 	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
 	// /**
